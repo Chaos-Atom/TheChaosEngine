f095451968e89f125b417064c12ab9f89a47c3f3
ChaosAtom <EntropyAtom@gmail.com>
2025-09-10 01:21:09 -0400

    First tiers of ore variants added, JEI compat. started

--- src/main/java/net/chaosatom/thechaosengine/TheChaosEngine.java
+++ src/main/java/net/chaosatom/thechaosengine/TheChaosEngine.java
@@ -1,5 +1,9 @@
 package net.chaosatom.thechaosengine;
 
+import net.chaosatom.thechaosengine.block.ModBlocks;
+import net.chaosatom.thechaosengine.item.ModCreativeModeTabs;
+import net.chaosatom.thechaosengine.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -33,6 +37,11 @@ public class TheChaosEngine {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
---
+++ src/main/java/net/chaosatom/thechaosengine/block/ModBlocks.java
@@ -0,0 +1,62 @@
+package net.chaosatom.thechaosengine.block;
+
+import com.mojang.serialization.MapCodec;
+import net.chaosatom.thechaosengine.TheChaosEngine;
+import net.chaosatom.thechaosengine.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(TheChaosEngine.MOD_ID);
+
+    // Block of Ore Dust
+    public static final DeferredBlock<Block> IRON_DUST_BLOCK = registerBlock("iron_dust_block",
+            () -> new FallingBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GRAY).strength(1f).sound(SoundType.SAND))
+                {
+                @Override
+                protected MapCodec<? extends FallingBlock> codec() {
+                    return null;
+                }
+                });
+    public static final DeferredBlock<Block> GOLD_DUST_BLOCK = registerBlock("gold_dust_block",
+            () -> new FallingBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).strength(1f).sound(SoundType.SAND))
+            {
+                @Override
+                protected MapCodec<? extends FallingBlock> codec() {
+                    return null;
+                }
+            });
+
+    public static final DeferredBlock<Block> COPPER_DUST_BLOCK = registerBlock("copper_dust_block",
+            () -> new FallingBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_ORANGE).strength(1f).sound(SoundType.SAND))
+            {
+                @Override
+                protected MapCodec<? extends FallingBlock> codec() {
+                    return null;
+                }
+            });
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
